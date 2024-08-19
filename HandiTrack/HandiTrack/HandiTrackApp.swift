514a90e507320a3cb232a487751de01ce9bb315f
Alfredo Sandoval-Luis <alfredo.sandovall@icloud.com>
2024-08-18 21:02:34 -0500

    initial commit of project

---
+++ HandiTrack/HandiTrack/HandiTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandiTrackApp.swift
+//  HandiTrack
+//
+//  Created by Alfredo Sandoval-Luis on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HandiTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
